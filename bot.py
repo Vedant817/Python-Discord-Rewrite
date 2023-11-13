196a3327e27d7c1057d97f51e7581a4eef403372
Vedant Mahajan <vedantmahajan271@gmail.com>
2023-11-13 16:24:23 +0530

    Adding embedding feature to the bot.

--- bot.py
+++ bot.py
@@ -22,13 +22,21 @@ async def update_stats():
                 f.write(f"Time: {int(time.time())}, Messages: {messages}, Members Joined: {joined} \n")
             messages = 0
             joined = 0
-
             await asyncio.sleep(5)
         except Exception as e:
             print(e)
             await asyncio.sleep(5)
 
-
+@client.event
+async def on_member_update(before, after): #* Before and After signifies the state of the Member.
+    n = after.nick #? Will be None if nickname is not being changed.
+    if n:
+        if n.lower().count("vedant") > 0:
+            last = before.nick
+            if last:
+                await after.edit(nick=last)
+            else:
+                await after.edit(nick="No Stop That")
 
 @client.event
 async def on_member_join(member):
@@ -42,9 +50,14 @@ async def on_member_join(member):
 async def on_message(message):
     global messages
     messages+=1
-    id = client.get_guild(1124087027487490168)
+    id = client.get_guild('CLIENT_ID')
     valid_users = ['.vedantmahajan']
     channels = ["commands"]
+    if message.content == "!help":
+        embed = discord.Embed(title="Help on Bot", description="Some Useful Commands")
+        embed.add_field(name="!hello", value="Greets the user")
+        embed.add_field(name="!users", value="Prints Number of Users")
+        await message.channel.send(embed=embed, content=None)
     #! For checking the valid user add the condition of str(message.author) in valid_users
     if str(message.channel) in channels:
         if message.content.find("!hello") != -1:
@@ -54,5 +67,11 @@ async def on_message(message):
     else:
         print(f"User: {message.author} tried to Command {message.content} in channel {message.channel}")
 
-client.loop.create_task(update_stats()) #? This is added to apply any background task in the channel.
+#! await message.channel.purge(limit=0-125)  To delete the message sent by the user.
+
+loop = asyncio.get_event_loop()
+try:
+    loop.create_task(update_stats()) #? This is added to apply any background task in the channel.
+except Exception as e:
+    print(f"Error occurring is {e}")
 client.run(token)
